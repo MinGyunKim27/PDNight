ca09ff1c74efd845aa7b58b65afc5623acc30075
김민균 <kmg02845@gmail.com>
2025-07-17 22:47:18 +0900

    Initial commit

---
+++ src/main/java/org/example/pdnight/PdNightApplication.java
@@ -0,0 +1,13 @@
+package org.example.pdnight;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PdNightApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PdNightApplication.class, args);
+    }
+
+}
