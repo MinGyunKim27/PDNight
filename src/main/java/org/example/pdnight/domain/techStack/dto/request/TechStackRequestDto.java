4a40c772ad4c4ca4ee853dc5edcb682debce5e51
김민균 <kmg02845@gmail.com>
2025-07-21 22:22:28 +0900

    feat(TechStack) : 테스트 코드 작성

--- src/main/java/org/example/pdnight/domain/techStack/dto/request/TechStackRequestDto.java
+++ src/main/java/org/example/pdnight/domain/techStack/dto/request/TechStackRequestDto.java
@@ -1,9 +1,11 @@
 package org.example.pdnight.domain.techStack.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class TechStackRequestDto {
     @NotNull
     private String techStack;
