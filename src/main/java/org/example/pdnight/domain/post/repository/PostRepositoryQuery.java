0d2ec5cc26466e73eb9042e1342ec10a8814b80a
teopteop <teopteop0718@gmail.com>
2025-07-21 12:28:57 +0900

    fix: 인터페이스에 붙은 리포지터리 어노테이션 제거

--- src/main/java/org/example/pdnight/domain/post/repository/PostRepositoryQuery.java
+++ src/main/java/org/example/pdnight/domain/post/repository/PostRepositoryQuery.java
@@ -12,7 +12,6 @@ import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Repository;
 
-@Repository
 public interface PostRepositoryQuery {
 
     Page<Post> getMyLikePost(Long userId, Pageable pageable);
@@ -27,6 +26,5 @@ public interface PostRepositoryQuery {
 		JobCategory jobCategoryLimit,
 		Gender genderLimit
 	);
-
 
 }
