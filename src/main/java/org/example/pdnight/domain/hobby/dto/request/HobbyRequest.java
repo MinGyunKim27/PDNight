630e1445ac5239a6306fe06b2b4e3708d1e61b6b
김민균 <kmg02845@gmail.com>
2025-07-21 22:22:42 +0900

    feat(Hobby) : 테스트 코드 작성

--- src/main/java/org/example/pdnight/domain/hobby/dto/request/HobbyRequest.java
+++ src/main/java/org/example/pdnight/domain/hobby/dto/request/HobbyRequest.java
@@ -1,9 +1,11 @@
 package org.example.pdnight.domain.hobby.dto.request;
 
 import jakarta.validation.constraints.NotNull;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
 
 @Getter
+@AllArgsConstructor
 public class HobbyRequest {
     @NotNull
     String hobby;
