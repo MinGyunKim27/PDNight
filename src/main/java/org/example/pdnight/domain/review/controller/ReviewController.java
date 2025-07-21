1646722843ee022283c95c399ec743f109f9e108
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-07-21 10:18:14 +0900

    feat: 유저아이디값 임의값 수정

--- src/main/java/org/example/pdnight/domain/review/controller/ReviewController.java
+++ src/main/java/org/example/pdnight/domain/review/controller/ReviewController.java
@@ -5,7 +5,9 @@ import org.example.pdnight.domain.common.dto.ApiResponse;
 import org.example.pdnight.domain.review.dto.request.ReviewRequestDto;
 import org.example.pdnight.domain.review.dto.response.ReviewResponseDto;
 import org.example.pdnight.domain.review.service.ReviewService;
+import org.example.pdnight.global.filter.CustomUserDetails;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.annotation.AuthenticationPrincipal;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -19,13 +21,12 @@ public class ReviewController {
 
     @PostMapping("/api/posts/{postId}/participants/{userId}/review")
     public ResponseEntity<ApiResponse<ReviewResponseDto>> createReview(
+            @AuthenticationPrincipal CustomUserDetails userDetails,
             @PathVariable Long postId,
             @PathVariable("userId") Long ratedUserId,
             @Validated @RequestBody ReviewRequestDto requestDto
     ) {
-        // todo: 추후 유저 @AuthenticationPrincipal를 통해 받아오기
-        Long userId = 1L;
-        ReviewResponseDto response = reviewService.createReview(userId, ratedUserId, postId, requestDto);
+        ReviewResponseDto response = reviewService.createReview(userDetails.getUserId(), ratedUserId, postId, requestDto);
 
         return ResponseEntity.ok(ApiResponse.ok("리뷰가 등록되었습니다.", response));
     }
