42e5a18d6e5f027daf0a9eeaf12bef7524e72a32
ryusu12 <rsy7567@gmail.com>
2025-07-21 09:05:09 +0900

    refactor: service 코드 간략화
    status로 신청목록을 조회하는 로직 반복
    - getParticipantByStatus 로 메서드화 및 사용

--- src/main/java/org/example/pdnight/domain/participant/service/ParticipantService.java
+++ src/main/java/org/example/pdnight/domain/participant/service/ParticipantService.java
@@ -20,8 +20,6 @@ import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @Service
 @RequiredArgsConstructor
 @Transactional(readOnly = true)
@@ -41,36 +39,22 @@ public class ParticipantService {
                 .orElseThrow(() -> new BaseException(HttpStatus.NOT_FOUND, "게시글을 찾을 수 없습니다"));
     }
 
-    private List<PostParticipant> getParticipantList(User user, Post post) {
-        return participantRepository.findByUserAndPost(user, post);
+    private PostParticipant getParticipantByStatus(User user, Post post, JoinStatus pending) {
+        return participantRepository.findByUserAndPost(user, post).stream()
+                .filter(p -> p.getStatus().equals(pending))
+                .findFirst()
+                .orElse(null);
     }
 
-    private Page<PostParticipant> getParticipantListByPostIdAndStatus(Post post, JoinStatus status, Pageable pageable) {
-        return participantRepository.findByPostAndStatus(post, status, pageable);
-    }
-
-    @Transactional
-    public ParticipantResponse applyParticipant(Long userId, Long postId) {
-        User user = getUser(userId);
-        Post post = getPost(postId);
-
+    private void validForCreateParticipant(User user, Post post) {
         // 신청 안됨 : 열려있는 게시글만 신청 가능 - PostStatus status 가 open 가 아닌경우
         if (post.getStatus() != PostStatus.OPEN) {
             throw new BaseException(HttpStatus.CONFLICT, "신청할 수 없습니다.");
         }
 
-        List<PostParticipant> postParticipant = getParticipantList(user, post);
-
         // 신청 안됨 : 이미 신청함 - JoinStatus status 가 대기중 or 수락됨 인 경우
-        PostParticipant pending = postParticipant.stream()
-                .filter(p -> p.getStatus().equals(JoinStatus.PENDING))
-                .findFirst()
-                .orElse(null);
-
-        PostParticipant accepted = postParticipant.stream()
-                .filter(p -> p.getStatus().equals(JoinStatus.ACCEPTED))
-                .findFirst()
-                .orElse(null);
+        PostParticipant pending = getParticipantByStatus(user, post, JoinStatus.PENDING);
+        PostParticipant accepted = getParticipantByStatus(user, post, JoinStatus.ACCEPTED);
 
         if (pending != null) {
             throw new BaseException(HttpStatus.CONFLICT, "이미 신청했습니다.");
@@ -78,6 +62,15 @@ public class ParticipantService {
         if (accepted != null) {
             throw new BaseException(HttpStatus.CONFLICT, "이미 가입되어있습니다.");
         }
+    }
+
+    @Transactional
+    public ParticipantResponse applyParticipant(Long userId, Long postId) {
+        User user = getUser(userId);
+        Post post = getPost(postId);
+
+        // 신청 안되는지 확인
+        validForCreateParticipant(user, post);
 
         // 정상 신청
         PostParticipant participant = PostParticipant.create(post, user);
@@ -98,11 +91,7 @@ public class ParticipantService {
         Post post = getPost(postId);
 
         // 삭제 안됨 : 신청하지 않거나, 이미 수락 혹은 거절당했을때
-        List<PostParticipant> postParticipant = getParticipantList(user, post);
-        PostParticipant pending = postParticipant.stream()
-                .filter(p -> p.getStatus().equals(JoinStatus.PENDING))
-                .findFirst()
-                .orElse(null);
+        PostParticipant pending = getParticipantByStatus(user, post, JoinStatus.PENDING);
 
         if (pending == null) {
             throw new BaseException(HttpStatus.CONFLICT, "취소할 수 없습니다.");
@@ -118,11 +107,7 @@ public class ParticipantService {
         Post post = getPost(postId);
         JoinStatus joinStatus = JoinStatus.of(status);
 
-        List<PostParticipant> postParticipant = getParticipantList(user, post);
-        PostParticipant pending = postParticipant.stream()
-                .filter(p -> p.getStatus().equals(JoinStatus.PENDING))
-                .findFirst()
-                .orElse(null);
+        PostParticipant pending = getParticipantByStatus(user, post, JoinStatus.PENDING);
 
         // 상태변경 안됨 : 신청 대기 상태가 아닐때
         if (pending == null || joinStatus.equals(JoinStatus.PENDING)) {
@@ -144,7 +129,7 @@ public class ParticipantService {
     public PagedResponse<ParticipantResponse> getParticipantListByStatus(Long postId, JoinStatus status, int page, int size) {
         Pageable pageable = PageRequest.of(page, size);
         Post post = getPost(postId);
-        Page<PostParticipant> postParticipant = getParticipantListByPostIdAndStatus(post, status, pageable);
+        Page<PostParticipant> postParticipant = participantRepository.findByPostAndStatus(post, status, pageable);
 
         return PagedResponse.from(postParticipant.map(p -> ParticipantResponse.of(
                 p.getUser().getId(),
