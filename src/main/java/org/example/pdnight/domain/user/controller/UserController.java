81fead2fe2b3f3a5d0c904516ab067d788c5547c
노트북 <were1117@gmail.com>
2025-07-21 11:58:33 +0900

    refactor : 유저 controller 반환값 수정 및 UserResponseDto 생성자 오류 수정

--- src/main/java/org/example/pdnight/domain/user/controller/UserController.java
+++ src/main/java/org/example/pdnight/domain/user/controller/UserController.java
@@ -7,6 +7,7 @@ import org.example.pdnight.domain.common.dto.ApiResponse;
 import org.example.pdnight.domain.user.dto.request.UserPasswordUpdateRequest;
 import org.example.pdnight.domain.user.dto.request.UserUpdateRequest;
 import org.example.pdnight.domain.user.service.UserService;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
@@ -17,61 +18,61 @@ public class UserController {
     private final UserService userService;
 
     @GetMapping("/my")
-    public ApiResponse<?> getMyProfile(
+    public ResponseEntity<ApiResponse<?>> getMyProfile(
             HttpServletRequest request
     ){
         Long userId = (Long) request.getAttribute("userId");
         // 로그인 중인 아이디로 프로필 조회
-        return ApiResponse.ok(
+        return ResponseEntity.ok(ApiResponse.ok(
                 "내 프로필이 조회되었습니다.",
                 userService.getMyProfile(userId)
-        );
+        ));
     }
 
     @PatchMapping("/my/profile")
-    public ApiResponse<?> updateMyProfile(
+    public ResponseEntity<ApiResponse<?>> updateMyProfile(
             HttpServletRequest request,
             @RequestBody UserUpdateRequest requestDto
     ){
         Long userId = (Long) request.getAttribute("userId");
 
-        return ApiResponse.ok(
+        return ResponseEntity.ok(ApiResponse.ok(
                 "프로필이 수정되었습니다.",
                 userService.updateMyProfile(userId, requestDto)
-        );
+        ));
     }
 
     @PatchMapping("/my/password")
-    public ApiResponse<?> updatePassword(
+    public ResponseEntity<ApiResponse<?>> updatePassword(
             HttpServletRequest request,
             @Valid @RequestBody UserPasswordUpdateRequest requestDto
     ){
         Long userId = (Long) request.getAttribute("userId");
         userService.updatePassword(userId, requestDto);
 
-        return ApiResponse.ok(
+        return ResponseEntity.ok(ApiResponse.ok(
                 "비밀번호가 수정되었습니다.",
                 null
-        );
+        ));
     }
 
     @GetMapping("/{id}")
-    public ApiResponse<?> getProfile(
+    public ResponseEntity<ApiResponse<?>> getProfile(
             @PathVariable Long id
     ){
-        return ApiResponse.ok(
+        return ResponseEntity.ok(ApiResponse.ok(
                 "프로필이 조회되었습니다.",
                 userService.getProfile(id, null)
-        );
+        ));
     }
 
     @GetMapping("/{id}/evaluation")
-    public ApiResponse<?> getEvaluation(
+    public ResponseEntity<ApiResponse<?> >getEvaluation(
             @PathVariable Long id
     ){
-        return ApiResponse.ok(
+        return ResponseEntity.ok(ApiResponse.ok(
                 "사용자 평가가 조회되었습니다.",
                 userService.getEvaluation(id)
-        );
+        ));
     }
 }
