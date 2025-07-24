04e34572ed86609959ca1e1bb48bb6e304198d88
teopteop <teopteop0718@gmail.com>
2025-07-24 19:45:07 +0900

    feat: admin 계정 자동생성 기능 추가
    - 프로젝트 DB 생성 시 1번 계정에 자동추가
    - 이후 ddl-auto : update 등으로 변경시 Email 검증 로직을 통해 중복 계정 생성 제한

---
+++ src/main/java/org/example/pdnight/global/init/AdminAccountInitializer.java
@@ -0,0 +1,33 @@
+package org.example.pdnight.global.init;
+
+import org.example.pdnight.domain.user.entity.User;
+import org.example.pdnight.domain.user.repository.UserRepository;
+import org.example.pdnight.global.config.PasswordEncoder;
+import org.springframework.boot.ApplicationArguments;
+import org.springframework.boot.ApplicationRunner;
+import org.springframework.stereotype.Component;
+
+import lombok.RequiredArgsConstructor;
+
+@Component
+@RequiredArgsConstructor
+public class AdminAccountInitializer implements ApplicationRunner {
+	private final UserRepository userRepository;
+	private final PasswordEncoder passwordEncoder;
+
+	@Override
+	public void run(ApplicationArguments args) throws Exception {
+		String adminEmail = "admin@pdnight.com";
+		String adminName = "관리자";
+		String adminPassword = passwordEncoder.encode("password1!");
+
+		//이미 admin 계정이 있을 시
+		if (userRepository.existsByEmail(adminEmail)) {
+			return;
+		}
+
+		User admin = User.createAdmin(adminEmail, adminName, adminPassword);
+		userRepository.save(admin);
+	}
+
+}
