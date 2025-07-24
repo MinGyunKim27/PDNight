412502716f91800ddeceb40a2fa579656b322ec1
ParkJin0814 <114732394+ParkJin0814@users.noreply.github.com>
2025-07-24 19:33:14 +0900

    fix: 맵핑 중복 삭제

--- src/main/java/org/example/pdnight/domain/user/controller/AdminUserController.java
+++ src/main/java/org/example/pdnight/domain/user/controller/AdminUserController.java
@@ -10,7 +10,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("/api/admin/users")
+@RequestMapping("/api/admin")
 @RequiredArgsConstructor
 public class AdminUserController {
     private final AdminUserService adminUserService;
