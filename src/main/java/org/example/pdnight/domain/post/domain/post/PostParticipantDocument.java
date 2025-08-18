0946a54a74981e2e0aed451e0208aec53bfc91a3
노트북 <were1117@gmail.com>
2025-08-18 17:54:33 +0900

    refactor: PostParticipantDocument LocalDateTime 필드에 @JsonFormat, @JsonSerialize 추가하여 형변환 되도록 수정

--- src/main/java/org/example/pdnight/domain/post/domain/post/PostParticipantDocument.java
+++ src/main/java/org/example/pdnight/domain/post/domain/post/PostParticipantDocument.java
@@ -1,6 +1,10 @@
 package org.example.pdnight.domain.post.domain.post;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.datatype.jsr310.ser.LocalDateTimeSerializer;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import org.example.pdnight.domain.post.enums.JoinStatus;
 import org.springframework.data.elasticsearch.annotations.Document;
 import org.springframework.data.elasticsearch.annotations.Field;
@@ -10,6 +14,7 @@ import java.time.LocalDateTime;
 
 @Document(indexName = "post_participants")
 @Getter
+@NoArgsConstructor
 public class PostParticipantDocument {
 
     private Long postId;
@@ -19,9 +24,13 @@ public class PostParticipantDocument {
     @Field(type = FieldType.Keyword)
     private JoinStatus status;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @Field(type = FieldType.Date, format = {}, pattern = "yyyy-MM-dd'T'HH:mm:ss||strict_date_time")
     private LocalDateTime createdAt;
 
+    @JsonFormat(pattern = "yyyy-MM-dd'T'HH:mm:ss")
+    @JsonSerialize(using = LocalDateTimeSerializer.class)
     @Field(type = FieldType.Date, format = {}, pattern = "yyyy-MM-dd'T'HH:mm:ss||strict_date_time")
     private LocalDateTime updatedAt;
 
