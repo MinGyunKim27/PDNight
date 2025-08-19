5fcd781f5d8f3af4b4ccc81906a022a3659202de
김민균 <kmg02845@gmail.com>
2025-08-19 14:48:16 +0900

    fix : elastic search 다른 주소에서도 접근 가능 하게 수정

--- src/main/java/org/example/pdnight/global/config/ElasticSearchConfig.java
+++ src/main/java/org/example/pdnight/global/config/ElasticSearchConfig.java
@@ -1,5 +1,7 @@
 package org.example.pdnight.global.config;
 
+import lombok.NonNull;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.elasticsearch.client.ClientConfiguration;
 import org.springframework.data.elasticsearch.client.elc.ElasticsearchConfiguration;
@@ -9,10 +11,17 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "org.springframework.data.elasticsearch.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
+    // connectedTo는 "host:port" 형식을 기대 (스킴 없이)
+    @Value("${spring.elasticsearch.uris:localhost:9200}")
+    private String uris; // 예: "3.37.21.115:9200" 또는 "host1:9200,host2:9200"
+
     @Override
+    @NonNull
     public ClientConfiguration clientConfiguration() {
+        String[] hosts = uris.replace(" ", "").split(",");
         return ClientConfiguration.builder()
-                .connectedTo("localhost:9200")
+                .connectedTo(hosts)   // 보안 OFF일 때
+                // .usingSsl()        // 보안 ON이면 사용 + 인증서 신뢰 설정 추가 필요
                 .build();
     }
 }
