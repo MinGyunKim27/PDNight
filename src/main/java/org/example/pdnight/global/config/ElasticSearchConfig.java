a153fdb8bdf0fbacb704691339d6c6e3b9ac3bf7
김민균 <kmg02845@gmail.com>
2025-08-19 14:57:02 +0900

    doc : 주석 작성

--- src/main/java/org/example/pdnight/global/config/ElasticSearchConfig.java
+++ src/main/java/org/example/pdnight/global/config/ElasticSearchConfig.java
@@ -11,17 +11,17 @@ import org.springframework.data.elasticsearch.repository.config.EnableElasticsea
 @EnableElasticsearchRepositories(basePackages = "org.springframework.data.elasticsearch.repository")
 public class ElasticSearchConfig extends ElasticsearchConfiguration {
 
-    // connectedTo는 "host:port" 형식을 기대 (스킴 없이)
+    //connectedTo 안에 들어갈 내용
     @Value("${spring.elasticsearch.uris:localhost:9200}")
-    private String uris; // 예: "3.37.21.115:9200" 또는 "host1:9200,host2:9200"
+    private String uris;
 
+    //Elastic Search와 연결
     @Override
     @NonNull
     public ClientConfiguration clientConfiguration() {
         String[] hosts = uris.replace(" ", "").split(",");
         return ClientConfiguration.builder()
-                .connectedTo(hosts)   // 보안 OFF일 때
-                // .usingSsl()        // 보안 ON이면 사용 + 인증서 신뢰 설정 추가 필요
+                .connectedTo(hosts)
                 .build();
     }
 }
